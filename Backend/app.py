659fc1027409f2cdabd860452675c13398d4190f
Deyika <205725921+Creykish@users.noreply.github.com>
2025-04-13 02:13:31 +1200

    getting ready for prod

--- Backend/app.py
+++ Backend/app.py
@@ -2,15 +2,18 @@ from calculation_engine.api import build_api
 from waitress import serve 
 from flask_cors import CORS
 import argparse
+import os
 
 arg_parser = argparse.ArgumentParser(description="Run the Flask web application.")
 
 app = build_api()
 CORS(app)
 
-arg_parser.add_argument("--port", type=int, default=5000, help="The port to run the web application on.")
-arg_parser.add_argument("--host", type=str, default="0.0.0.0", help="The host to run the web application on.")
-args = arg_parser.parse_args()
+if __name__ == "__main__":
+    default_port = int(os.environ.get("PORT", 8080))  # Use env PORT, fallback to 8080
+    arg_parser.add_argument("--port", type=int, default=5000, help="The port to run the web application on.")
+    arg_parser.add_argument("--host", type=str, default="0.0.0.0", help="The host to run the web application on.")
+    args = arg_parser.parse_args()
 
-print(f"Starting server on {args.host}:{args.port}")
-serve(app, host=args.host, port=args.port)
+    print(f"Starting server on {args.host}:{args.port}")
+    serve(app, host=args.host, port=args.port)
